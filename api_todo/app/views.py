b7e517ab39e8f4f7075645d6f8f283456f431460
Joilton R. Silva <dev.joiltonrsilva@protonmail.com>
2022-06-29 18:55:06 -0300

    Add: Upload Files

---
+++ api_todo/app/views.py
@@ -0,0 +1,11 @@
+from app.models import Todo
+from app.serializers import TodoSerializer
+
+
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework import viewsets
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
